58d06cef47b8bc60632a92eac4629e21ffb461e4
seunghwakim <54671765+kimseunghwa102@users.noreply.github.com>
2020-06-16 19:32:11 +0900

    Update maps.js

--- maps.js
+++ maps.js
@@ -16,8 +16,8 @@ map.addSource('places', {
 'type': 'Feature',
 'properties': {
 'description':
-'<strong>Muelek（ムーレック）</strong><p><a href="http://www.kyotodekuraso.com/p/5732" target="_blank" title="Opens in a new window">ムーレック</a>は,アジアを主にする世界の子供支援するを目的とした、フェアトレードのカフェと雑貨屋です。雑貨はどれも手作りで、カフェは築65年の小さな庭を持つ屋です。営業時間	11:00～18:00 ,定休日	水曜日・木曜日,TEL 075-462-3311</p>',
-'icon': 'theatre'
+'<strong>Muelek（ムーレック）</strong><p><a href="http://www.kyotodekuraso.com/p/5732" target="_blank" title="Opens in a new window">ムーレック</a>は,アジアを主にする世界の子供支援するを目的とした、フェアトレードのカフェと雑貨屋です。雑貨はどれも手作りで、カフェは築65年の小さな庭を持つ屋です。<br>営業時間	11:00～18:00 <br>定休日 : 水曜日・木曜日<br>TEL 075-462-3311</p>',
+'icon': 'rocket'
 },
 'geometry': {
 'type': 'Point',
@@ -28,60 +28,60 @@ map.addSource('places', {
 'type': 'Feature',
 'properties': {
 'description':
-'<strong>Mad Men Season Five Finale Watch Party</strong><p>Head to Lounge 201 (201 Massachusetts Avenue NE) Sunday for a <a href="http://madmens5finale.eventbrite.com/" target="_blank" title="Opens in a new window">Mad Men Season Five Finale Watch Party</a>, complete with 60s costume contest, Mad Men trivia, and retro food and drink. 8:00-11:00 p.m. $10 general admission, $20 admission and two hour open bar.</p>',
-'icon': 'theatre'
-},
-'geometry': {
-'type': 'Point',
-'coordinates': [-77.003168, 38.894651]
-}
-},
-{
-'type': 'Feature',
-'properties': {
-'description':
-'<strong>Big Backyard Beach Bash and Wine Fest</strong><p>EatBar (2761 Washington Boulevard Arlington VA) is throwing a <a href="http://tallulaeatbar.ticketleap.com/2012beachblanket/" target="_blank" title="Opens in a new window">Big Backyard Beach Bash and Wine Fest</a> on Saturday, serving up conch fritters, fish tacos and crab sliders, and Red Apron hot dogs. 12:00-3:00 p.m. $25.grill hot dogs.</p>',
-'icon': 'bar'
-},
-'geometry': {
-'type': 'Point',
-'coordinates': [-77.090372, 38.881189]
-}
-},
-{
-'type': 'Feature',
-'properties': {
-'description':
-'<strong>Ballston Arts & Crafts Market</strong><p>The <a href="http://ballstonarts-craftsmarket.blogspot.com/" target="_blank" title="Opens in a new window">Ballston Arts & Crafts Market</a> sets up shop next to the Ballston metro this Saturday for the first of five dates this summer. Nearly 35 artists and crafters will be on hand selling their wares. 10:00-4:00 p.m.</p>',
-'icon': 'art-gallery'
-},
-'geometry': {
-'type': 'Point',
-'coordinates': [-77.111561, 38.882342]
-}
-},
-{
-'type': 'Feature',
-'properties': {
-'description':
-'<strong>Seersucker Bike Ride and Social</strong><p>Feeling dandy? Get fancy, grab your bike, and take part in this year\'s <a href="http://dandiesandquaintrelles.com/2012/04/the-seersucker-social-is-set-for-june-9th-save-the-date-and-start-planning-your-look/" target="_blank" title="Opens in a new window">Seersucker Social</a> bike ride from Dandies and Quaintrelles. After the ride enjoy a lawn party at Hillwood with jazz, cocktails, paper hat-making, and more. 11:00-7:00 p.m.</p>',
-'icon': 'bicycle'
-},
-'geometry': {
-'type': 'Point',
-'coordinates': [-77.052477, 38.943951]
-}
-},
-{
-'type': 'Feature',
-'properties': {
-'description':
-'<strong>Capital Pride Parade</strong><p>The annual <a href="http://www.capitalpride.org/parade" target="_blank" title="Opens in a new window">Capital Pride Parade</a> makes its way through Dupont this Saturday. 4:30 p.m. Free.</p>',
+'<strong>楽天堂(自然食品販売・塾)</strong><p> <a href="https://www.rakutendo.com/index.html" target="_blank" title="Opens in a new window">楽天堂の願いは、</a>世界中の人がこぞって肉を食べれば食糧危機は深刻になるばかりだけど、世界中の人が満ち足りた食事ができるように―これが楽天堂・豆料理クラブの願いです。<br>TEL 075-811-4890,FAX 02046656740,<br>営業日：水～土曜（日～火曜＆祝日休),<br> 営業時間：11-7時</p>',
 'icon': 'rocket'
 },
 'geometry': {
 'type': 'Point',
-'coordinates': [-77.043444, 38.909664]
+'coordinates': [135.737726, 35.020399]
+}
+},
+{
+'type': 'Feature',
+'properties': {
+'description':
+'<strong>Globe Mountain Coffee</strong><p><a href="https://shop.globe-mountain-coffee.com/" target="_blank" title="Opens in a new window">Globe Mountain Coffeeは、</a> フェアトレード・オーガニックコーヒー専門店。京都に店舗を構える自家焙煎コーヒーショップです。アジア系のコーヒーを中心に取り揃えています。焙煎したての香り高いコーヒーをお届けします。是非ご利用下さい。<br>TEL075-812-0606<br>営業日：木曜日＆祝日休　<br> 営業時：10時から23時まで</p>',
+'icon': 'rocket'
+},
+'geometry': {
+'type': 'Point',
+'coordinates': [135.732054, 35.012978]
+}
+},
+{
+'type': 'Feature',
+'properties': {
+'description':
+'<strong>リボーン(フェアトレードお服販売)</strong><p><a href="http://www.reborn-kyoto.org/" target="_blank" title="Opens in a new window">リボーンは、</a> 日本全国から寄贈された着物地を活用し、主に洋裁の技術指導を通じて途上国の女性の精神的経済的自立を支援している非営利団体。<br>営業時間	10:00～18:00 <br>定休日 : 土曜日・日曜日<br>TEL 075-213-1731</p>',
+'icon': 'rocket'
+},
+'geometry': {
+'type': 'Point',
+'coordinates': [135.755739, 35.007573]
+}
+},
+{
+'type': 'Feature',
+'properties': {
+'description':
+'<strong>株式会社 Hibana</strong><p><a href="https://hibana.co.jp/" target="_blank" title="Opens in a new window">株式会社Hibanaは、</a><br>弊社は「森林バイオマスの利用促進」を目的として2006年に設立しました。バイオマスとは、生物（bio）由来の物質（mass）のことを差し、中でも木が由来の資源を森林（木質）バイオマスと言います。木は、素材として利用するだけでなく、燃焼させてエネルギーとして利用することもできます。そして何より持続可能な資源。「環境へやさしい資源」として、今注目を集めています。<br>営業時間	10:00～19:00 <br>定休日 : 水曜日<br>TEL 075-241-6038</p>',
+'icon': 'rocket'
+},
+'geometry': {
+'type': 'Point',
+'coordinates': [135.767359, 35.012882]
+}
+},
+{
+'type': 'Feature',
+'properties': {
+'description':
+'<strong>sisam</strong><p><a href="https://sisam.jp/" target="_blank" title="Opens in a new window">sisamは、</a>1999年の創業以来、フェアトレードに事業として携わっています。フェアトレードとはお買いものによって社会経済的に弱い立場にいる人たちの暮らしを支える仕組みであり、そのフェアトレード商品や考え方を広めていくこと自体が社会的なアクションだと考えています。<br>TEL075-212-1653<br> 営業時：11:30時から21時まで<br>住所：〒604-8041 京都府京都市中京区裏寺町 通蛸薬師上ル裏寺町591</p>',
+'icon': 'rocket'
+},
+'geometry': {
+'type': 'Point',
+'coordinates': [135.768425, 35.006383]
 }
 },
 {
@@ -163,3 +163,4 @@ map.on('mouseleave', 'places', function() {
 map.getCanvas().style.cursor = '';
 });
 });
+
