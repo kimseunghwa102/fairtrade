83f70ebbd2190b7278d8eb43e877a2696a660dff
seunghwakim <54671765+kimseunghwa102@users.noreply.github.com>
2020-07-03 16:49:36 +0900

    Update maps.js

--- maps.js
+++ maps.js
@@ -1,6 +1,6 @@
 //지도검색용 체크하기 코드
 mapboxgl.accessToken = 'pk.eyJ1IjoiZmFraHIiLCJhIjoiY2pseXc0djE0MHBibzN2b2h4MzVoZjk4aSJ9.ImbyLtfsfSsR_yyBluR8yQ';
-// 풀 스크린과 드라이브 디력선 코드 믿에 1개
+// 풀 스크린과,드라이브 디력선 코드 믿에 1개, 맵글씨쓰기
 mapboxgl.accessToken = 'pk.eyJ1Ijoia2ltc2V1bmdod2EiLCJhIjoiY2tiZ2dwYmlxMTVoZDJ1bWVseXg2dWFreiJ9.aiGGvnGULguEMU_wDX2xSg';
 var instructions = document.getElementById('instructions');
 var map = new mapboxgl.Map({
@@ -10,13 +10,15 @@ var map = new mapboxgl.Map({
     zoom: 10, // starting zoom　
     minZoom: 1 // keep it local
 });
-//드라이브 디렉션
-map.addControl(
-new MapboxDirections({
-accessToken: mapboxgl.accessToken
-}),
-'top-left'
-);
+
+
+//드라이브 디렉션 네비게이션 
+// map.addControl(
+// new MapboxDirections({
+// accessToken: mapboxgl.accessToken
+// }),
+// 'top-left'
+// );
 // 풀 스크린 코드
 map.addControl(new mapboxgl.FullscreenControl()
 );
@@ -45,8 +47,8 @@ map.on('load', function() {
 'type': 'Feature',
 'properties': {
 'description':
-'<strong>Muelek（ムーレック）</strong><p><a href="http://www.kyotodekuraso.com/p/5732" target="_blank" title="Opens in a new window">ムーレック</a>は,アジアを主にする世界の子供支援するを目的とした、フェアトレードのカフェと雑貨屋です。雑貨はどれも手作りで、カフェは築65年の小さな庭を持つ屋です。<br>営業時間	11:00～18:00 <br>定休日 : 水曜日・木曜日<br>TEL 075-462-3311<br>住所：京都府京都市北区等持院南町12-3</p>',
-'icon': 'rocket'
+'<strong>Muelek（雑貨屋andカフェ）</strong><a href="quiz.html" class="btn messagequiz">Quiz</a><p><a href="http://www.kyotodekuraso.com/p/5732" target="_blank" title="Opens in a new window">ムーレック</a>は,アジアを主にする世界の子供支援するを目的とした、フェアトレードのカフェと雑貨屋です。雑貨はどれも手作りで、カフェは築65年の小さな庭を持つ屋です。<br>営業時間	11:00～18:00 <br>定休日 : 水曜日・木曜日<br>TEL 075-462-3311<br>住所：京都府京都市北区等持院南町12-3</p>',
+'icon': 'cafe'
 },
 'geometry': {
 'type': 'Point',
@@ -58,7 +60,7 @@ map.on('load', function() {
 'properties': {
 'description':
 '<strong>楽天堂(自然食品販売・塾)</strong><p> <a href="https://www.rakutendo.com/index.html" target="_blank" title="Opens in a new window">楽天堂の願いは、</a>世界中の人がこぞって肉を食べれば食糧危機は深刻になるばかりだけど、世界中の人が満ち足りた食事ができるように―これが楽天堂・豆料理クラブの願いです。<br>TEL 075-811-4890,FAX 02046656740,<br>営業日：水～土曜（日～火曜＆祝日休),<br> 営業時間：11-7時<br>住所：〒602-8354 京都府京都市上京区西東町３６４−１４</p>',
-'icon': 'rocket'
+'icon': 'cafe'
 },
 'geometry': {
 'type': 'Point',
@@ -70,7 +72,7 @@ map.on('load', function() {
 'properties': {
 'description':
 '<strong>Globe Mountain Coffee</strong><p><a href="https://shop.globe-mountain-coffee.com/" target="_blank" title="Opens in a new window">Globe Mountain Coffeeは、</a> フェアトレード・オーガニックコーヒー専門店。京都に店舗を構える自家焙煎コーヒーショップです。アジア系のコーヒーを中心に取り揃えています。焙煎したての香り高いコーヒーをお届けします。是非ご利用下さい。<br>TEL075-812-0606<br>営業日：木曜日＆祝日休　<br> 営業時：10時から23時まで<br>住所：〒604-8437 京都府京都市中京区西ノ京東中合町５５ ハイツウエストロード</p>',
-'icon': 'rocket'
+'icon': 'cafe'
 },
 'geometry': {
 'type': 'Point',
@@ -82,7 +84,7 @@ map.on('load', function() {
 'properties': {
 'description':
 '<strong>リボーン(フェアトレードお服販売)</strong><p><a href="http://www.reborn-kyoto.org/" target="_blank" title="Opens in a new window">リボーンは、</a> 日本全国から寄贈された着物地を活用し、主に洋裁の技術指導を通じて途上国の女性の精神的経済的自立を支援している非営利団体。<br>営業時間	10:00～18:00 <br>定休日 : 土曜日・日曜日<br>TEL 075-213-1731<br>住所：〒604-8217 京都府京都市中京区西六角町１０１</p>',
-'icon': 'rocket'
+'icon': 'clothing-store'
 },
 'geometry': {
 'type': 'Point',
@@ -94,7 +96,7 @@ map.on('load', function() {
 'properties': {
 'description':
 '<strong>株式会社 Hibana</strong><p><a href="https://hibana.co.jp/" target="_blank" title="Opens in a new window">株式会社Hibanaは、</a><br>弊社は「森林バイオマスの利用促進」を目的として2006年に設立しました。バイオマスとは、生物（bio）由来の物質（mass）のことを差し、中でも木が由来の資源を森林（木質）バイオマスと言います。木は、素材として利用するだけでなく、燃焼させてエネルギーとして利用することもできます。そして何より持続可能な資源。「環境へやさしい資源」として、今注目を集めています。<br>営業時間	10:00～19:00 <br>定休日 : 水曜日<br>TEL 075-241-6038<br>住所：〒604-0931 京都府京都市中京区 寺町二条下榎木98−7</p>',
-'icon': 'rocket'
+'icon': 'park'
 },
 'geometry': {
 'type': 'Point',
@@ -106,7 +108,7 @@ map.on('load', function() {
 'properties': {
 'description':
 '<strong>sisam裏寺通店</strong><p><a href="https://sisam.jp/" target="_blank" title="Opens in a new window">sisamは、</a>1999年の創業以来、フェアトレードに事業として携わっています。フェアトレードとはお買いものによって社会経済的に弱い立場にいる人たちの暮らしを支える仕組みであり、そのフェアトレード商品や考え方を広めていくこと自体が社会的なアクションだと考えています。<br>TEL075-212-1653<br> 営業時：11:30時から21時まで<br>住所：〒604-8041 京都府京都市中京区裏寺町 通蛸薬師上ル裏寺町591</p>',
-'icon': 'rocket'
+'icon': 'clothing-store'
 },
 'geometry': {
 'type': 'Point',
@@ -118,7 +120,7 @@ map.on('load', function() {
 'properties': {
 'description':
 '<strong>JIPPO(COFFEE)</strong><p><a href="https://jippo.or.jp/">JIPPO</a>は、現地調査が必要。<br>検証されていない。<br>住所：〒600-8358 京都府京都市下京区本願寺門前町</p>',
-'icon': 'rocket'
+'icon': 'cafe'
 },
 'geometry': {
 'type': 'Point',
@@ -130,7 +132,7 @@ map.on('load', function() {
 'properties': {
 'description':
 '<strong>Kitten Company Cafe</strong><p><a href="http://kittencompany.net/" target="_blank" title="Opens in a new window"><em>Kitten Company Cafe</em></a>は、キトゥンカンパニー は…、京都・五条烏丸でオーガニックな食材のドリンク・フードメニューを提供していてる茶店。ベジタリアンやビーガンのみんなにもニッコリ楽しんでもらえます。<br>TEL075-344-1591<br> 営業時：11:00時から21時まで(土日は休み)<br>住所：〒600-8170 京都府京都市下京区五条烏丸西入る上諏訪町２９４−１</p>',
-'icon': 'rocket'
+'icon': 'cafe'
 },
 'geometry': {
 'type': 'Point',
@@ -154,7 +156,7 @@ map.on('load', function() {
 'properties': {
 'description':
 '<strong>京都・本店 SISAM</strong><p><a href="https://sisam.jp/">SISAM本店</a>は、1999年の創業以来、フェアトレードに事業として携わっています。フェアトレードとはお買いものによって社会経済的に弱い立場にいる人たちの暮らしを支える仕組みであり、そのフェアトレード商品や考え方を広めていくこと自体が社会的なアクションだと考えています。<br>TEL:075-724-5688<br> 営業時：11:00時から19:30時まで<br>住所：〒606-8224 京都府京都市左京区北白川追分町８０−１</p>',
-'icon': 'rocket'
+'icon': 'clothing-store'
 },
 'geometry': {
 'type': 'Point',
@@ -166,7 +168,7 @@ map.on('load', function() {
 'properties': {
 'description':
 '<strong>なやカフェ</strong><p><a href="https://twitter.com/naya_cafe">なやカフェは</a>現場検証が必要。<br>TEL075-<br> 営業時：12:00から20:00まで。<br>火曜日だけオープン<br>住所:〒606-8277 京都府京都市左京区北白川堂ノ前町１ デュ北白川102</p>',
-'icon': 'rocket'
+'icon': 'cafe'
 },
 'geometry': {
 'type': 'Point',
@@ -178,7 +180,7 @@ map.on('load', function() {
 'properties': {
 'description':
 '<strong>キッチンハリーナ（フェアトレード食堂）（新店舗）</strong><p><a href="https://kitchen-halina.jimdofree.com/">キッチンハリーナは</a>、季節のごはんとゆったり空間、よそではちょっと手に入らない選りすぐりのフェアトレードグッズが自慢です。フェアトレードのコーヒー、紅茶、バナナ、塩、砂糖を使ってい、食事を提供。<br>TEL090-9993-9447<br> 営業時：12:00時から19:00時まで(日、月曜日休み土曜日は、18:00から22:00まで)<br>住所:〒606-8267 京都府京都市左京区北白川西町８５−５</p>',
-'icon': 'rocket'
+'icon': 'restaurant'
 },
 'geometry': {
 'type': 'Point',
@@ -190,7 +192,7 @@ map.on('load', function() {
 'properties': {
 'description':
 '<strong>キッチンハリーナ（フェアトレード食堂）（本店）</strong><p><a href="https://kitchen-halina.jimdofree.com/">キッチンハリーナは</a>、季節のごはんとゆったり空間、よそではちょっと手に入らない選りすぐりのフェアトレードグッズが自慢です。フェアトレードのコーヒー、紅茶、バナナ、塩、砂糖を使ってい、食事を提供。<br>TEL090-9993-9447<br> 営業時：12:00時から19:00時まで(日、月曜日休み土曜日は、18:00から22:00まで)<br>住所:〒606-8267 京都府京都市左京区左京区北白川西町８５−４</p>',
-'icon': 'rocket'
+'icon': 'clothing-store'
 },
 'geometry': {
 'type': 'Point',
